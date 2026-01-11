f7a79470952c20e4d5f724847fa52f9b9243075f
d <dsandalledu@gmail.com>
2026-01-11 13:29:31 -0800

    support for ocaml, in prep for hardcaml

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 -- require() starts in your nvim/lua/ directory
-require("config.lazy")         -- /nvim/lua/config/lazy.lua
+require("config.lazy") -- /nvim/lua/config/lazy.lua
 require("osc52_ssh_clipboard") -- /nvim/lua/osc52_ssh_clipboard.lua
 require("shift_arrow_windows")
