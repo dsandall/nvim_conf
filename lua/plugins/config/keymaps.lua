2a32bd09b02c3d7d197e5bc679da861e70b31f54
Dylan Sandall <dsandalledu@gmail.com>
2026-02-09 12:08:32 -0800

    add inlay hints binding

--- lua/plugins/config/keymaps.lua
+++ lua/plugins/config/keymaps.lua
@@ -4,4 +4,11 @@ vim.keymap.set("n", "<S-Down>", "<C-w>j", { desc = "Go to bottom window" })
 vim.keymap.set("n", "<S-Up>", "<C-w>k", { desc = "Go to top window" })
 vim.keymap.set("n", "<S-Right>", "<C-w>l", { desc = "Go to right window" })
 
+local toggleInlay = function()
+	local current_value = vim.lsp.inlay_hint.is_enabled({ bufnr = 0 })
+	vim.lsp.inlay_hint.enable(not current_value, { bufnr = 0 })
+end
+
+vim.keymap.set("n", "<leader>j", toggleInlay, { desc = "lsp inlay hints" })
+
 return {}
