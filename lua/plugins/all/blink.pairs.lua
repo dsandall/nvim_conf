1bf0f8c59f8a48ac5f9e3c376dd7ed31a958d114
Dylan Sandall <dsandalledu@gmail.com>
2026-02-15 12:54:55 -0800

    quickscope, disable auto-paren matching

--- lua/plugins/all/blink.pairs.lua
+++ lua/plugins/all/blink.pairs.lua
@@ -17,7 +17,7 @@ return {
 			-- you can call require("blink.pairs.mappings").enable()
 			-- and require("blink.pairs.mappings").disable()
 			-- to enable/disable mappings at runtime
-			enabled = true,
+			enabled = false,
 			cmdline = true,
 			-- or disable with `vim.g.pairs = false` (global) and `vim.b.pairs = false` (per-buffer)
 			-- and/or with `vim.g.blink_pairs = false` and `vim.b.blink_pairs = false`
