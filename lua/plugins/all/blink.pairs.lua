51e55b5264b7bca68cf37c928d30d21364068ae3
Dylan Sandall <dsandalledu@gmail.com>
2026-02-22 21:10:50 -0800

    fix blink bug

--- lua/plugins/all/blink.pairs.lua
+++ lua/plugins/all/blink.pairs.lua
@@ -3,7 +3,7 @@ return {
 	version = "*", -- (recommended) only required with prebuilt binaries
 
 	-- download prebuilt binaries from github releases
-	dependencies = "saghen/blink.download",
+	-- dependencies = "saghen/blink.download",
 	-- OR build from source, requires nightly:
 	-- https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
 	-- build = 'cargo build --release',
@@ -13,19 +13,7 @@ return {
 	--- @module 'blink.pairs'
 	--- @type blink.pairs.Config
 	opts = {
-		mappings = {
-			-- you can call require("blink.pairs.mappings").enable()
-			-- and require("blink.pairs.mappings").disable()
-			-- to enable/disable mappings at runtime
-			enabled = false,
-			cmdline = true,
-			-- or disable with `vim.g.pairs = false` (global) and `vim.b.pairs = false` (per-buffer)
-			-- and/or with `vim.g.blink_pairs = false` and `vim.b.blink_pairs = false`
-			disabled_filetypes = {},
-			-- see the defaults:
-			-- https://github.com/Saghen/blink.pairs/blob/main/lua/blink/pairs/config/mappings.lua#L14
-			pairs = {},
-		},
+		mappings = { enabled = false },
 		highlights = {
 			enabled = true,
 			-- requires require('vim._extui').enable({}), otherwise has no effect
