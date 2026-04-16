3789f06e7a0d193094ceae294b28fa22737a563c
Dylan Sandall <dsandalledu@gmail.com>
2026-04-15 21:43:34 -0700

    initial neo-tree git 'most recently edited' view

--- lua/plugins/all/neo-tree.lua
+++ lua/plugins/all/neo-tree.lua
@@ -23,14 +23,73 @@ return {
 	keys = {
 		{ "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Toggle Neo-tree" },
 	},
-	opts = {
-		icon_provider = "mini.icons",
-		filesystem = {
-			follow_current_file = {
-				enabled = true,
-				leave_dirs_open = true,
+	config = function()
+		-- cache: path -> date string, or false if not in git / untracked
+		local git_date_cache = {}
+		local refresh_timer = nil
+
+		local function schedule_refresh()
+			-- debounce: batch all async completions into one redraw
+			if refresh_timer then
+				refresh_timer:stop()
+				refresh_timer:close()
+			end
+			refresh_timer = vim.defer_fn(function()
+				refresh_timer = nil
+				require("neo-tree.sources.manager").refresh("filesystem")
+			end, 150)
+		end
+
+		require("neo-tree").setup({
+			icon_provider = "mini.icons",
+			filesystem = {
+				follow_current_file = {
+					enabled = true,
+					leave_dirs_open = true,
+				},
+				use_libuv_file_watcher = true,
+				components = {
+					git_last_modified = function(config, node, _state)
+						local path = node:get_id()
+						local cached = git_date_cache[path]
+
+						if cached == nil then
+							-- mark in-flight to avoid duplicate jobs
+							git_date_cache[path] = false
+							vim.system(
+								{ "git", "log", "-1", "--format=%cr", "--", path },
+								{ text = true },
+								vim.schedule_wrap(function(result)
+									local date = (result.stdout or ""):gsub("%s+", "")
+									git_date_cache[path] = date ~= "" and date or false
+									if date ~= "" then schedule_refresh() end
+								end)
+							)
+							return {}
+						end
+
+						if not cached then return {} end
+						return { text = cached, highlight = config.highlight or "NeoTreeDimText" }
+					end,
+				},
+				renderers = {
+					file = {
+						{ "indent" },
+						{ "icon" },
+						{ "filtered_by" },
+						{ "name", use_git_status_colors = true },
+						{ "git_status", highlight = "NeoTreeDimText" },
+						{ "git_last_modified", highlight = "NeoTreeDimText" },
+					},
+					directory = {
+						{ "indent" },
+						{ "icon" },
+						{ "filtered_by" },
+						{ "name" },
+						{ "git_last_modified", highlight = "NeoTreeDimText" },
+					},
+				},
 			},
-			use_libuv_file_watcher = true,
-		},
-	},
+		})
+	end,
 }
